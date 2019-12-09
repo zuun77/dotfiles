d8162d07ebbb92fbd9abd200bdf0fb8b54a495ea
Ubuntu <ubuntu@ip-172-31-44-242.ap-northeast-2.compute.internal>
2019-12-09 10:53:50 +0000

    Update symlink target path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE}")";
 
@@ -6,13 +6,13 @@ git pull origin master;
 
 function doIt() {
 	# files
-	for file in {.bash_profile,.aliases,.functions,.macosx,.zshrc}; do
-		[ -r "$file" ] && [ -f "$file" ] && ln -sv "$(pwd)/$file" ~/testdir;
+	for file in {.bash_profile,.aliases,.functions,.macosx,.zshrc,.vimrc}; do
+		[ -r "$file" ] && [ -f "$file" ] && ln -sv "$(pwd)/$file" ~;
 	done;
 
 	# directories
 	for dir in {.vim,init}; do
-		ln -sv "$(pwd)/$dir" ~/testdir;
+		ln -sv "$(pwd)/$dir" ~;
 	done;
 }
 
